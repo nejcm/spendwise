1bb7ffd3397fc21e306293e0714dcf077961be6d
nejcm <nejc.mu@gmail.com>
2026-04-03 13:33:29 +0800

    style: remove border from input

--- src/features/transactions/components/transaction-form.tsx
+++ src/features/transactions/components/transaction-form.tsx
@@ -180,7 +180,7 @@ export function TransactionForm({ initialValues, onSuccess, onCancel, isSheet }:
                     testID="base-amount-input"
                     error={getFieldError(field)}
                     containerClassName="min-w-[72] flex-1"
-                    className="border-none bg-transparent"
+                    className="border-0 bg-transparent"
                   />
                 )}
               />
